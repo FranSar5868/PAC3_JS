9f730af0e4a41aab5ba018114f7fca666b8320fc
FranSar5868 <fsarrias@coit.es>
2023-05-09 21:57:09 +0200

    I add an event listener to the cards

--- script.js
+++ script.js
@@ -34,25 +34,32 @@ async function fetchRandomPokemon() {
     Promise.all(promises)
     .then(results => {
       const container = document.getElementById("container");
-      results.forEach(pokemon => {
+      results.forEach((pokemon,i) => {
         const card = document.createElement("div");
         card.classList.add("card");
+        card.setAttribute("id", `pokemon-container-$(i)`);
   
         const name = document.createElement("h2");
         name.textContent = pokemon.name;
   
         const image = document.createElement("img");
         image.src = pokemon.sprites.front_default;
+        image.setAttribute("alt", pokemon.name);
   
         card.appendChild(name);
         card.appendChild(image);
         container.appendChild(card);
+        card.addEventListener("click", () => {
+          console.log(`Card clicked for Pokemon with ID ${i}`);
+        });
       });
+     
+      
     })
     .catch(error => console.error(error));
   
   
-  
+   
 
     
     // Promise.all(promises)
